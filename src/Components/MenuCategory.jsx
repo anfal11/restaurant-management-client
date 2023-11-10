43009a93952a68bafe3f2fd53c166ad7d857d1bc
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-11 03:56:02 +0600

    menu and menucat updated

--- src/Components/MenuCategory.jsx
+++ src/Components/MenuCategory.jsx
@@ -1,8 +1,19 @@
+import Cover from "../Shared/Cover";
+import MenuItem from "../Shared/MenuItem";
 
-const MenuCategory = () => {
+
+const MenuCategory = ({items, title, coverImg}) => {
     return (
-        <div>
-            
+        <div className="px-2">
+        { title && <Cover img={coverImg} title={title}></Cover>}
+            <section className="grid md:grid-cols-2 gap-3 mt-16">
+                {
+                    items.map (item => <MenuItem key={item._id} item = {item}></MenuItem>)
+                }
+            </section>
+            <div className="flex items-center justify-center my-3">
+           <button className="btn btn-outline border-0 border-b-4">ORDER YOUR FAVOURITE FOOD</button>
+           </div>
         </div>
     );
 };
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,16 +1,48 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover";
 import menuImg from "../assets/menu/banner3.jpg";
+import dessertImg from "../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../assets/menu/pizza-bg.jpg";
+import saladImg from "../assets/menu/salad-bg.jpg";
+import soupImg from "../assets/menu/soup-bg.jpg";
+import useMenu from "../Hooks/useMenu";
+import SectionTitle from "../Components/SectionTitle";
+import MenuCategory from "../Components/MenuCategory";
 
 
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const offered = menu.filter(item => item.category === 'offered');
+
   return (
     <div>
       <Helmet>
         <title>Tavern Restaurant | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="Our Menu"></Cover>
+      {/* main cover  */}
+      <SectionTitle
+      heading={"Don't miss"}
+        subHeading={"TODAY'S OFFER"}>
+      </SectionTitle>
+      {/* offered menu items */}
+
+      <MenuCategory items={offered}></MenuCategory>
+      {/* dessert menu items */}
+      
+      <MenuCategory items={dessert} title="Dessert" coverImg={dessertImg}></MenuCategory>
+
+      <MenuCategory items={pizza} title="Pizza" coverImg={pizzaImg}></MenuCategory>
+
+      <MenuCategory items={salad} title="Salad" coverImg={saladImg}></MenuCategory>
+
+      <MenuCategory items={soup} title="Soup" coverImg={soupImg}></MenuCategory>
+
     </div>
   );
 };
