408642cf1487f4f1b94a76f37810d7218eda3d01
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-11 04:11:36 +0600

    react tab installed

--- src/Pages/Order.jsx
+++ src/Pages/Order.jsx
@@ -1,21 +1,35 @@
 import { Helmet } from "react-helmet-async";
 import order from "../assets/shop/banner2.jpg";
 import Cover from "../Shared/Cover";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Order = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>Tavern Restaurant | Order</title>
-            </Helmet>
 
-            <section>
-                <Cover img={order} title={'ORDER FOOD'}>
+  return (
+    <div>
+      <Helmet>
+        <title>Tavern Restaurant | Order</title>
+      </Helmet>
 
-                </Cover>
-            </section>
-        </div>
-    );
+      <section>
+        <Cover img={order} title={"ORDER FOOD"}></Cover>
+        <Tabs>
+    <TabList>
+      <Tab>Title 1</Tab>
+      <Tab>Title 2</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2>Any content 1</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+  </Tabs>
+      </section>
+    </div>
+  );
 };
 
 export default Order;
