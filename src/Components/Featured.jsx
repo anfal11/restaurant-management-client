8bf70c0e25f77fd6be6768d6452f6740d28a715e
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-10 20:56:41 +0600

    testimonials created and featured completed

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,23 +1,25 @@
 import SectionTitle from "./SectionTitle";
 import featured from '../../src/assets/home/featured.jpg'
+import './Featured.css'
 
 
 const Featured = () => {
     return (
-        <div>
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle
             heading='Check it out'
             subHeading='Featured Item'
             >
             </SectionTitle>
-            <div>
+            <div className="bg-slate-600 opacity-60 max-w-7xl mx-auto gap-2 flex flex-col md:flex-row justify-center items-center py-8 px-16">
                 <div>
-                    <img src={featured} alt="" />
+                    <img className="w-[500px]" src={featured} alt="" />
                 </div>
-                <div>
+                <div className="ml-10 space-y-2">
                 <p>March 20, 2023</p>
                 <p>WHERE CAN I GET SOME?</p>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Error <br />voluptate facere, deserunt dolores maiores quod nobis quas quasi. <br />Eaque repellat recusandae ad laudantium tempore consequatur <br />consequuntur omnis ullam maxime tenetur.
+                <p>Explore our signature dishes, crafted with precision. Delight in the fusion of flavors at our culinary haven. Indulge in a gastronomic journey!</p>
+                <button className="btn btn-outline border-0 border-b-4 text-white">Order Now</button>
                 </div>
             </div>
         </div>
---
+++ src/Components/Testimonials.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import SectionTitle from "./SectionTitle";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const Testimonials = () => {
+
+    useEffect(()=>{
+
+    },[])
+
+    return (
+        <div>
+            <SectionTitle heading='---What Our Clients Say---' subHeading='TESTIMONIALS'></SectionTitle>
+        </div>
+    );
+};
+
+export default Testimonials;
