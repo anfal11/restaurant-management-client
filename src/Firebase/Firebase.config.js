faa07f35d109583ba9ba30f0006663828cda5fda
anfal11 <anfal15-14303@diu.edu.bd>
2023-11-13 02:44:16 +0600

    firebase config done

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5m0NdCUVpDc3SXj_uttWChUg4Q7UOPjM",
-  authDomain: "restaurant-management-f3524.firebaseapp.com",
-  projectId: "restaurant-management-f3524",
-  storageBucket: "restaurant-management-f3524.appspot.com",
-  messagingSenderId: "409266746256",
-  appId: "1:409266746256:web:f1fb7805a9aed60ca5f915"
+  apiKey: import.meta.env.VITE_apikey,
+  authDomain: import.meta.env.VITE_authdomain,
+  projectId: import.meta.env.VITE_projectid,
+  storageBucket: import.meta.env.VITE_storagebucket,
+  messagingSenderId: import.meta.env.VITE_messagingsenderid,
+  appId: import.meta.env.VITE_appid
 };
 
 // Initialize Firebase
